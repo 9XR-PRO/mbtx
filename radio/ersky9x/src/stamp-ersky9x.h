bd90a050924a5c276b7f4ce766dd0942fa9eda8e
René Post <rene@xpt.nl>
2017-07-08 00:20:49 +0200

    Change what information is shown on version screen.

---
+++ radio/ersky9x/src/stamp-ersky9x.h
@@ -0,0 +1,16 @@
+
+// Name under which the firmware is known.
+#define FW_NAME "ErSky9x"
+
+// Adapt semantic versioning for the code r220 -> 2.2.0
+// Code should be tagged with v2.2.0 so we can generate GIT_HASH based on that.
+#define SEM_VER "2.2.0"
+
+// Date when the firmware was build
+#define DATE_STR "2017-07-07"
+
+// Time when the firmware was build
+#define TIME_STR "17:07:07"
+
+// Short git hash for the tag base on SEM_VERS i.e. git rev-parse --short v2.2.0
+#define GIT_HASH "badbeef"
